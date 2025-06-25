dfa5e15db667f78345e1b07e179ae873cf06546b
ElevatechLLP2016 <elevatechpune@gmail.com>
2025-06-25 11:18:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ if pin.value() is 0:
 #main code here
 TIME_MS=100
 LED = Pin("LED", Pin.OUT)
+LED1 = Pin(4, Pin.OUT)
 while True:
     LED.off()
+    LED1.on()
     time.sleep_ms(TIME_MS)
     LED.on()
+    LED1.off()
     time.sleep_ms(TIME_MS)
